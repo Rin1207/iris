947fa3618212fc9c89f37ef71b4f991b2ce25150
Rin <rynuhf@gmail.com>
2025-09-08 11:22:52 -0700

    on school iPad

---
+++ commands/slash/utility/jailSetup.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, PermissionBitsField } = require('discord.js');
+
+module.exports = {
+     data: new SlashCommandBuilder()
+         .setName('jailsetup')
+         .setDescription('setups the jail system'),
+
+     async execute(interaction) {
+          const jailChannel = interaction.channel.create('jail');
+          const jailRole = interaction.guild.roles.create({
+            name: 'jail',
+          });
+     }
+
+
+    interaction.reply('created jail channel and jail role');
+}
